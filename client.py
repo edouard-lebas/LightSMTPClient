bd3797fc6dda6cd43bb9b6563799f7eda4c7f036
Edouard LEBAS <15814419+edouard-lebas@users.noreply.github.com>
2022-07-18 10:28:29 +0200

    Refactor & UI improvments

--- client.py
+++ client.py
@@ -1,58 +1,68 @@
 from tkinter import *
 
-window = Tk()
-window.title('LightSMTPClient')
-window.geometry('400x600')
+class LightSMTPClient(Frame):
 
-config_frame = LabelFrame(window, text="Configuration", padx=20, pady=20)
-config_frame.pack(fill="both", expand="yes", padx=20, pady=20)
+    def __init__(self,window):
+        Frame.__init__(self, window)
+        self.window = window
+        self.initUI()
 
-message_frame = LabelFrame(window, text="Message", padx=20, pady=20)
-message_frame.pack(fill="both", expand="yes",padx=20, pady=20)
+    def initUI(self):
+        config_frame = LabelFrame(window, text="Configuration", padx=20, pady=20)
+        config_frame.grid(column=0, row=0, padx=20, pady=20)
 
-#SERVER
-server_label = Label(config_frame, text="SMTP server")
-server_label.pack()
-server_data = StringVar()
-server_input = Entry(config_frame, textvariable=server_data, width=30)
-server_input.pack()
+        message_frame = LabelFrame(window, text="Message", padx=20, pady=20)
+        message_frame.grid(column=0, row=2,padx=20, pady=20)
 
-#PORT
-port_label = Label(config_frame, text="Port")
-port_label.pack()
-port_data = StringVar()
-port_input = Entry(config_frame, textvariable=port_data, width=10)
-port_input.pack()
+        #SERVER
+        server_label = Label(config_frame, text="Server")
+        server_label.grid(column=0, row=1)
+        server_data = StringVar()
+        server_input = Entry(config_frame, textvariable=server_data, width=30)
+        server_input.grid(column=1, row=1,padx=5, pady=5)
 
-#FROM
-from_label = Label(message_frame, text="From")
-from_label.pack()
-from_data = StringVar()
-from_input = Entry(message_frame, textvariable=from_data, width=30)
-from_input.pack()
+        #PORT
+        port_label = Label(config_frame, text="Port")
+        port_label.grid(column=0, row=2)
+        port_data = StringVar()
+        port_input = Entry(config_frame, textvariable=port_data, width=30)
+        port_input.grid(column=1, row=2,padx=5, pady=5)
 
-#TO
-to_label = Label(message_frame, text="To")
-to_label.pack()
-to_data = StringVar()
-to_input = Entry(message_frame, textvariable=to_data, width=30)
-to_input.pack()
+        #FROM
+        from_label = Label(message_frame, text="From")
+        from_label.grid(column=0, row=3)
+        from_data = StringVar()
+        from_input = Entry(message_frame, textvariable=from_data, width=30)
+        from_input.grid(column=1, row=3,padx=5, pady=5)
 
-#SUBJECT
-subject_label = Label(message_frame, text="Subject")
-subject_label.pack()
-subject_data = StringVar()
-subject_input = Entry(message_frame, textvariable=subject_data, width=30)
-subject_input.pack()
+        #TO
+        to_label = Label(message_frame, text="To")
+        to_label.grid(column=0, row=4)
+        to_data = StringVar()
+        to_input = Entry(message_frame, textvariable=to_data, width=30)
+        to_input.grid(column=1, row=4,padx=5, pady=5)
 
-#SUBJECT
-message_label = Label(message_frame, text="Message")
-message_label.pack()
-body_data = StringVar()
-body_input = Entry(message_frame, textvariable=body_data, width=30)
-body_input.pack()
+        #SUBJECT
+        subject_label = Label(message_frame, text="Subject")
+        subject_label.grid(column=0, row=5)
+        subject_data = StringVar()
+        subject_input = Entry(message_frame, textvariable=subject_data, width=30)
+        subject_input.grid(column=1, row=5,padx=5, pady=5)
 
-send_button=Button(window, text="Send")
-send_button.pack(padx=20, pady=20)
+        #MESSAGE
+        message_label = Label(message_frame, text="Message")
+        message_label.grid(column=0, row=6)
+        body_data = StringVar()
+        body_input = Text(message_frame, height=10, width=30)
+        body_input.grid(column=1, row=6,padx=5, pady=5)
 
-window.mainloop()
+        send_button=Button(window, text="Send")
+        send_button.grid(column=0, row=10, padx=20, pady=20)
+
+if __name__ == "__main__":
+    window = Tk()
+    LightSMTPClient(window)
+    window.title('LightSMTPClient')
+    window.geometry('')
+
+    window.mainloop()
